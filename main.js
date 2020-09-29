c7e7d87a88c708a0b7f99684b0a862d9380c816f
Yuri Bachkov <unrealevil@gmail.com>
2020-09-29 18:53:07 +0300

    Home work 4

--- main.js
+++ main.js
@@ -1,64 +1,97 @@
 const $btnKick = document.getElementById('btn-kick');
 const $btnSuperKick = document.getElementById('btn-super-kick');
+const $btnStartGame = document.getElementById('btn-start-game');
+
+const render = function () {
+    this.elHP.innerText = this.damageHP + ' / ' + this.defaultHP;
+    const hpPercent = Math.round(this.damageHP / this.defaultHP * 100);
+    this.elProgressbar.style.width = hpPercent + '%';
+}
+
+const makeDamage = function (amount) {
+    this.damageHP -= amount;
+    if (this.damageHP < 0) {
+        this.damageHP = 0;
+    }
+    this.render();
+}
+
+const isDead = function () {
+    return 0 === this.damageHP;
+}
+
+const reset = function () {
+    this.damageHP = this.defaultHP;
+    this.render();
+}
 
 const character = {
     name: 'Pikachu',
-    defaultHP: 100,
-    damageHP: 100,
-    elHP:  document.getElementById('health-character'),
+    defaultHP: 150,
+    damageHP: 0,
+    elHP: document.getElementById('health-character'),
     elProgressbar: document.getElementById('progressbar-character'),
+    render: render,
+    makeDamage: makeDamage,
+    isDead: isDead,
+    reset: reset,
 }
 
 const enemy = {
     name: 'Charmander',
-    defaultHP: 100,
-    damageHP: 100,
-    elHP:  document.getElementById('health-enemy'),
+    defaultHP: 300,
+    damageHP: 0,
+    elHP: document.getElementById('health-enemy'),
     elProgressbar: document.getElementById('progressbar-enemy'),
+    render: render,
+    makeDamage: makeDamage,
+    isDead: isDead,
+    reset: reset,
 }
 
-function init() {
-    renderHP(character);
-    renderHP(enemy);
+function startGame() {
+    $btnStartGame.style.display = 'none';
+    $btnKick.style.display = 'block';
+    $btnSuperKick.style.display = 'block';
+    character.reset();
+    enemy.reset();
+}
+
+function checkEndGameCondition() {
+    if (character.isDead()) {
+        endGame();
+        alert('You Lost');
+        return;
+    }
+    if (enemy.isDead()) {
+        endGame();
+        alert('You Win');
+    }
+}
+
+function endGame() {
+    $btnKick.style.display = 'none';
+    $btnSuperKick.style.display = 'none';
+    $btnStartGame.style.display = 'block';
 }
 
 $btnKick.addEventListener('click', function () {
-    changeHP(random(20), character);
-    changeHP(random(20), enemy);
+    character.makeDamage(random(20));
+    enemy.makeDamage(random(20));
+    checkEndGameCondition();
 });
 
 $btnSuperKick.addEventListener('click', function () {
-    changeHP(random(50), enemy);
+    enemy.makeDamage(random(50));
+    checkEndGameCondition();
 });
 
-function changeHP(damage, person) {
-    person.damageHP -= damage;
-    if(person.damageHP < 0){
-        person.damageHP = 0;
-    }
-    renderHP(person);
-    if(0 === person.damageHP){
-        $btnKick.disabled = true;
-        $btnSuperKick.disabled = true;
-        alert(person.name + ' lose');
-    }
-}
-
-function renderHP(person) {
-    renderHPLife(person);
-    renderProgressBarHP(person);
-}
-
-function renderHPLife(person) {
-    person.elHP.innerText = person.damageHP + ' / ' + person.defaultHP;
-}
-
-function renderProgressBarHP(person) {
-    person.elProgressbar.style.width = person.damageHP + '%';
-}
+$btnStartGame.addEventListener('click', function () {
+    startGame();
+});
 
 function random(max) {
     return Math.ceil(Math.random() * max);
 }
 
-init();
+endGame();
